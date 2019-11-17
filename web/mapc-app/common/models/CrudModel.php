8b0b9520586c136fa1cc9ae0ee9328bda15e8f37
wooyg <wooyg@wooyg.com>
2019-11-18 00:12:55 +0900

    파일정리.20191118

--- web/mapc-app/common/models/CrudModel.php
+++ web/mapc-app/common/models/CrudModel.php
@@ -2,18 +2,45 @@
 namespace Mapc\Common;
 
 /**
- * Bare Model
+ * Cure Bare Model
  * @version 0.1
  */
-class Bare {
+class Crud {
 
-    public $uuid;
-/*
-    public function create() {}
-    public function read() {}
+	public $db;
+
+	public $table;
+    public $id;
+    public $vars;
+
+    public function __construct($args = []) {
+
+        $this->db = $args['db'];
+        $this->db->fancyDebug(true);
+        $this->vars = $this->db->getRedBean()->dispense($args['table']);
+        $this->table = $args['table'];
+
+    }
+
+    public function create() {
+
+        $this->id = $this->db->store($this->vars);
+
+        return $this->id;
+        
+    }
+
+    public function retrieve($id) {
+
+    	$result = $this->db->load($this->table, $id);
+
+    	return $result;
+
+    }
+
     public function update() {}
     public function delete() {}
-*/
+
 } // class
 
 // this is it
