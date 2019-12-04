a15b54183e1a4dea617948daba5540294ef74878
jhchu <jhchu@enliple.com>
2019-12-04 18:07:36 +0900

    1. oAuth composer.json에 추가 2. access_token 발급까지 작업완료

--- web/mapc-app/oAuth/models/oAuthModel.php
+++ web/mapc-app/oAuth/models/oAuthModel.php
@@ -1,13 +1,40 @@
 <?php
-namespace Mapc\Rankbest;
+namespace Mapc;
+//namespace Mapc\oAuth2;
 
-use Mapc\Common\Crud;
+use OAuth2;
+//use OAuth2\Autoloader;
 
 /**
  * Item Model
  * @version 0.1
  */
-class oAuth extends Crud {
+class oAuth {
+
+    function getConfig() {
+        $dsn      = 'mysql:dbname=mysql;host=localhost';
+        $username = 'root';
+        $password = 'root';
+
+        // error reporting (this is a demo, after all!)
+        ini_set('display_errors',1);error_reporting(E_ALL);
+
+        // Autoloading (composer is preferred, but for this example let's just do this)
+
+        OAuth2\Autoloader::register();
+
+        // $dsn is the Data Source Name for your database, for exmaple "mysql:dbname=my_oauth2_db;host=localhost"
+        $storage = new OAuth2\Storage\Pdo(array('dsn' => $dsn, 'username' => $username, 'password' => $password));
+
+        // Pass a storage object or array of storage objects to the OAuth2 server class
+        $server = new OAuth2\Server($storage);
+
+        // Add the "Client Credentials" grant type (it is the simplest of the grant types)
+        $server->addGrantType(new OAuth2\GrantType\ClientCredentials($storage));
+
+        // Add the "Authorization Code" grant type (this is where the oauth magic happens)
+        $server->addGrantType(new OAuth2\GrantType\AuthorizationCode($storage));
+    }
 
 } // class
 
