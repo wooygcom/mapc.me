fabfe0e6ed1b3692ddf35f186a9e4d0069cb4d78
jhchu <jhchu@enliple.com>
2019-12-05 15:40:47 +0900

    oAuthModel 파일 정리

--- web/mapc-app/oAuth/controllers/authorizeController.php
+++ web/mapc-app/oAuth/controllers/authorizeController.php
@@ -1,39 +1,11 @@
 <?php
 if(!defined("__MAPC__")) { exit(); }
 
-@include($ROUTES['callback'] . '.php');
+include PROC_PATH . 'proc.autoload.php';
 
-// include our OAuth2 Server object
-// require_once __DIR__.'/server/server.php';
-######################## model로 옮기기 s ########################
-use OAuth2\Autoloader;
+use Mapc\oAuth\oAuth;
 
-$dsn      = 'mysql:dbname=mysql;host=localhost';
-$username = 'root';
-$password = 'root';
-
-// error reporting (this is a demo, after all!)
-ini_set('display_errors',1);error_reporting(E_ALL);
-
-// Autoloading (composer is preferred, but for this example let's just do this)
-
-OAuth2\Autoloader::register();
-
-// $dsn is the Data Source Name for your database, for exmaple "mysql:dbname=my_oauth2_db;host=localhost"
-$storage = new OAuth2\Storage\Pdo(array('dsn' => $dsn, 'username' => $username, 'password' => $password));
-
-// Pass a storage object or array of storage objects to the OAuth2 server class
-$server = new OAuth2\Server($storage);
-
-// Add the "Client Credentials" grant type (it is the simplest of the grant types)
-$server->addGrantType(new OAuth2\GrantType\ClientCredentials($storage));
-
-// Add the "Authorization Code" grant type (this is where the oauth magic happens)
-$server->addGrantType(new OAuth2\GrantType\AuthorizationCode($storage));
-######################## model로 옮기기 e ########################
-
-
-
+$server = oAuth::setConfig();
 
 
 $request = OAuth2\Request::createFromGlobals();
@@ -44,22 +16,10 @@ if (!$server->validateAuthorizeRequest($request, $response)) {
     $response->send();
     die;
 }
-// display an authorization form
-/*if (empty($_POST)) {
-    exit('
-<form method="post">
-  <label>Do You Authorize TestClient?</label><br />
-  <input type="submit" name="authorized" value="yes">
-  <input type="submit" name="authorized" value="no">
-</form>');
-}*/
 
-// print the authorization code if the user has authorized your client
-// $is_authorized = ($_POST['authorized'] === 'yes');
 $is_authorized = true;
 $server->handleAuthorizeRequest($request, $response, $is_authorized);
 if ($is_authorized) {
-    // this is only here so that you get to see your code in the cURL request. Otherwise, we'd redirect back to the client
     $code = substr($response->getHttpHeader('Location'), strpos($response->getHttpHeader('Location'), 'code=')+5, 40);
     exit("SUCCESS! Authorization Code: $code");
 }
--- web/mapc-app/oAuth/models/oAuthModel.php
+++ web/mapc-app/oAuth/models/oAuthModel.php
@@ -1,13 +1,63 @@
 <?php
-namespace Mapc\Rankbest;
+namespace Mapc\oAuth;
 
 use Mapc\Common\Crud;
+use OAuth2\Autoloader;
+use OAuth2\GrantType\ClientCredentials;
+use OAuth2\Server;
+use OAuth2\Storage\Pdo;
+use OAuth2\GrantType\AuthorizationCode;
+use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
+use League\OAuth2\Client\Provider\GenericProvider;
 
 /**
  * Item Model
  * @version 0.1
  */
 class oAuth extends Crud {
+
+    public function getUrl(){
+        $url = "http://localhost/web/mapc-public/";
+        return $url;
+    }
+
+    public function clientInfo($postArr = []){
+        $url = "http://localhost/web/mapc-public/";
+        $clientId = 'testclient';
+        $clientSecret = 'testpass';
+        $clientInfo = [
+            'clientId'  => $clientId,
+            'clientSecret'  => $clientSecret,
+            'redirectUri'   => $url . 'oAuth/server'
+        ];
+        if(isset($postArr['user_email']) && $postArr['user_email']){
+            $clientInfo['clientId'] = $postArr['user_email'];
+        }
+        if(isset($postArr['user_password']) && $postArr['user_password']){
+            $clientInfo['clientSecret'] = $postArr['user_password'];
+        }
+        if(isset($postArr['redirect_uri']) && $postArr['redirect_uri']){
+            $clientInfo['redirectUri'] = $postArr['redirect_uri'];
+        }
+        return $clientInfo;
+    }
+
+    public function setConfig()
+    {
+
+        $dsn = 'mysql:dbname=mysql;host=localhost';
+        $username = 'root';
+        $password = 'root';
+        ini_set('display_errors', 1);
+        error_reporting(E_ALL);
+
+        Autoloader::register();
+        $storage = new Pdo(array('dsn' => $dsn, 'username' => $username, 'password' => $password));
+        $server = new Server($storage);
+        $server->addGrantType(new ClientCredentials($storage));
+        $server->addGrantType(new AuthorizationCode($storage));
+        return $server;
+    }
 
 } // class
 
