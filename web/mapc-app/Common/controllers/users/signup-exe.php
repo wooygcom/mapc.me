e957a28660dfbd775736848c3b62fc2e0a630f7e
WooYG <wooyg@wooyg.com>
2020-03-31 23:16:14 +0900

    signup

---
+++ web/mapc-app/Common/controllers/users/signup-exe.php
@@ -0,0 +1,1 @@
+signup-exe.php
---
+++ web/mapc-app/Common/views/users/signup.php
@@ -0,0 +1,53 @@
+<?php
+$layout = 'metro4';
+
+include(LAYOUT_PATH . $layout . '/head.php');
+include(LAYOUT_PATH . $layout . '/header.php');
+?>
+
+    <form class="login-form bg-white p-6 mx-auto border bd-default win-shadow"
+          data-role="validator"
+          action=""
+          method="post"
+          data-clear-invalid="2000"
+          data-on-error-form="invalidForm"
+          data-on-validate-form="validateForm">
+        <input type="hidden" name="_csrf" value="<?= $_SESSION['csrf']; ?>" />
+        <input type="hidden" name="_method" value="update" /><!-- POST, PUT, PATCH, DELETE -->
+        <span class="mif-vpn-lock mif-4x place-right" style="margin-top: -10px;"></span>
+        <h2 class="text-light"><?php echo '로그인'; ?></h2>
+        <hr class="thin mt-4 mb-4 bg-white">
+        <div class="form-group">
+            <input name="userid" type="text" data-role="input" data-prepend="<span class='mif-envelop'>" placeholder="Enter your email..." data-validate="required email">
+        </div>
+        <div class="form-group">
+            <input name="userpasswd" type="password" data-role="input" data-prepend="<span class='mif-key'>" placeholder="Enter your password..." data-validate="required minlength=6">
+        </div>
+        <div class="form-group mt-10">
+            <input type="checkbox" data-role="checkbox" data-caption="Remember me" class="place-right">
+            <button class="button">Submit form</button>
+        </div>
+    </form>
+
+    <script src="<?= ROOT_URL; ?>vendor/metro4/build/js/metro.js"></script>
+    <script>
+        function invalidForm(){
+            var form  = $(this);
+            form.addClass("ani-ring");
+            setTimeout(function(){
+                form.removeClass("ani-ring");
+            }, 1000);
+        }
+
+        function validateForm(){
+            $(".login-form").animate({
+                opacity: 0
+            });
+        }
+    </script>
+
+<?php
+include(LAYOUT_PATH . $layout . '/footer.php');
+include(LAYOUT_PATH . $layout . '/foot.php');
+
+// this is it
